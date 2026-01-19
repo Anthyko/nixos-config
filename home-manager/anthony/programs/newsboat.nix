e8cc639beb5670da46a07ba72dfb209d9b45fcba
Anthyko <16465475+Anthyko@users.noreply.github.com>
2026-01-19 20:21:09 +0100

    added new url to newsboat

--- home-manager/anthony/programs/newsboat.nix
+++ home-manager/anthony/programs/newsboat.nix
@@ -15,6 +15,8 @@
       { url = "https://www.bleepingcomputer.com/feed/"; }
       { url = "https://www.nicematin.com/loisirs/festivites/rss"; }
       { url = "https://wiki.nixos.org/w/api.php?hidebots=1&translations=filter&urlversion=1&days=7&limit=50&action=feedrecentchanges&feedformat=atom"; }
+  { url = "https://www.youtube.com/feeds/videos.xml?channel_id=UC1KxoDAzbWOWOhw5GbsE-Bw";}
+  { url = "https://www.youtube.com/feeds/videos.xml?channel_id=UCO5QSoES5yn2Dw7YixDYT5Q";}
     ];
   };
 }
