2d1e6744e5c10a92932d09af53525a13399f3b12
Anthyko <16465475+Anthyko@users.noreply.github.com>
2026-01-28 17:08:16 +0100

    updated audio config

--- nixos-configs/zeno/modules/audio.nix
+++ nixos-configs/zeno/modules/audio.nix
@@ -18,4 +18,12 @@
       };
     };
   };
+
+  #   Disable power saving for the snd_hda_intel driver.
+  # Prevents audio clicks, pops, and crackling (underruns) during gaming or when audio resumes.
+  # Recommended for gaming and real-time audio usage with PipeWire.
+  boot.extraModprobeConfig = ''
+    options snd_hda_intel power_save=0
+  '';
+
 }
