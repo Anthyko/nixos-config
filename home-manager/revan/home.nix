b4893bdd502858d4163ef1264682c36f295bbefd
Anthyko <16465475+Anthyko@users.noreply.github.com>
2026-01-29 19:53:00 +0100

    updated revan to use the generic zsh config

--- home-manager/revan/home.nix
+++ home-manager/revan/home.nix
@@ -34,28 +34,16 @@
     k9s
     stdenv.cc.cc.lib
   ];
-  imports = [ ];
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
 
   home.sessionVariables = {
     EDITOR = "nvim";
     LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
   };
-
+  imports = [
+    (import ../common/programs/zsh-base.nix {
+      extraAliases = { };
+    })
+  ];
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
