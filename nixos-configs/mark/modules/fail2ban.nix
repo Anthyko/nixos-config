54598a4293475afe77d5c86d4cf0b3861becfe88
Anthyko <16465475+Anthyko@users.noreply.github.com>
2026-02-14 09:56:01 +0100

    fixed url probe jail

--- nixos-configs/mark/modules/fail2ban.nix
+++ nixos-configs/mark/modules/fail2ban.nix
@@ -2,7 +2,13 @@
 , pkgs
 , ...
 }: {
-  environment.etc = {
+   environment.etc = {
+    # Define an action that will trigger a Ntfy push notification upon the issue of every new ban
+    "fail2ban/action.d/ntfy.local".text = pkgs.lib.mkDefault (pkgs.lib.mkAfter ''
+      [Definition]
+      norestored = true # Needed to avoid receiving a new notification after every restart
+      actionban = curl -H "Title: <ip> has been banned" -d "<name> jail has banned <ip> from accessing $(hostname) after <failures> attempts of hacking the system." https://ntfy.sh/Fail2banNotifications
+    '');
     # Defines a filter that detects URL probing by reading the Nginx access log
     "fail2ban/filter.d/nginx-url-probe.local".text = pkgs.lib.mkDefault (pkgs.lib.mkAfter ''
       [Definition]
