9b980d8baab5e7358066d9680f13807025334649
Anthyko <16465475+Anthyko@users.noreply.github.com>
2026-02-18 19:20:35 +0100

    nfmt

--- nixos-configs/common-modules/sops.nix
+++ nixos-configs/common-modules/sops.nix
@@ -2,8 +2,8 @@
 , pkgs
 , ...
 }: {
-           sops.defaultSopsFile = ../../secrets/secrets.yaml;
-           sops.defaultSopsFormat = "yaml";
-           sops.age.keyFile = "/var/lib/sops-nix/keys.txt";
+  sops.defaultSopsFile = ../../secrets/secrets.yaml;
+  sops.defaultSopsFormat = "yaml";
+  sops.age.keyFile = "/var/lib/sops-nix/keys.txt";
 
 }
