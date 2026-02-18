d1a94870d9a87e43ebf7bca1d1e609943cf22a69
Anthyko <16465475+Anthyko@users.noreply.github.com>
2026-02-18 19:32:39 +0100

    setting secrets for mumble

--- nixos-configs/mark/modules/murmur.nix
+++ nixos-configs/mark/modules/murmur.nix
@@ -2,8 +2,17 @@
 , pkgs
 , ...
 }: {
+  sops.secrets."murmur/env" = {
+    sopsFile = ../../../secrets/murmur.env;
+    format = "dotenv";
+    owner = "murmur";
+    group = "murmur";
+    mode = "0400";
+  };
   services.murmur = {
     enable = true;
+    environmentFile = config.sops.secrets."murmur/env".path;
     openFirewall = true;
+    password = "$MURMURD_PASSWORD";
   };
 }
