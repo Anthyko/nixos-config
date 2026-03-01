db2a954cdc2a10855925777b12bf3e8877ee6b32
Anthyko <16465475+Anthyko@users.noreply.github.com>
2026-03-01 13:22:14 +0100

    updated flake to use niri files

--- flake.nix
+++ flake.nix
@@ -77,6 +77,8 @@
         stylix.nixosModules.stylix
         ./nixos-configs/common-modules/hyprland.nix
         ./nixos-configs/common-modules/desktop-env.nix
+        ./nixos-configs/common-modules/niri.nix
+
       ];
 
       hmCommonBaseModules = [
@@ -86,6 +88,7 @@
 
       hmCommonDesktopModules = [
         ./home-manager/common/programs/hyprland.nix
+        ./home-manager/common/programs/niri.nix
         ./home-manager/common/programs/foot.nix
         ./home-manager/common/programs/mpv.nix
       ];
