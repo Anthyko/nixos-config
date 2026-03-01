04d56e2f96ee04bfc8a3557e224a3396c2fa568e
Anthyko <16465475+Anthyko@users.noreply.github.com>
2026-03-01 08:32:17 +0100

    updated flake

--- nixos-configs/common-modules/desktop-env.nix
+++ nixos-configs/common-modules/desktop-env.nix
@@ -5,5 +5,5 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
+  services.displayManager.sddm.wayland.enable = false;
 }
