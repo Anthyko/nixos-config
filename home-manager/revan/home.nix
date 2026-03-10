6e769fdc10e51a545012ffa2f47cab0d8d49ff2f
Anthony <16465475+Anthyko@users.noreply.github.com>
2026-03-10 14:09:14 +0100

    Update home.nix

--- home-manager/revan/home.nix
+++ home-manager/revan/home.nix
@@ -41,7 +41,7 @@
   };
   imports = [
     (import ../common/programs/zsh-base.nix {
-      extraAliases = { };
+      extraAliases = { hmrs = "nh home switch --accept-flake-config ~/nixos-config -c revan" };
     })
   ];
   # Let Home Manager install and manage itself.
