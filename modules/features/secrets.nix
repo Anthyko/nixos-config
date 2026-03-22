137c8838e98d20d6c32ee65b885baf9fdc97e1bd
Anthyko <16465475+Anthyko@users.noreply.github.com>
2026-03-22 08:45:48 +0100

    nfmt

--- modules/features/secrets.nix
+++ modules/features/secrets.nix
@@ -4,8 +4,8 @@
 }:
 {
   flake.nixosModules.secrets = { pkgs, ... }: {
-  sops.defaultSopsFile = ../../secrets/secrets.yaml;
-  sops.defaultSopsFormat = "yaml";
-  sops.age.keyFile = "/var/lib/sops-nix/keys.txt";
+    sops.defaultSopsFile = ../../secrets/secrets.yaml;
+    sops.defaultSopsFormat = "yaml";
+    sops.age.keyFile = "/var/lib/sops-nix/keys.txt";
   };
 }
