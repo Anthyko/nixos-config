e55d8667964433a73baf5270b4eac4e6d5f00ac5
Anthyko <16465475+Anthyko@users.noreply.github.com>
2026-03-22 08:36:52 +0100

    nfmt

--- modules/features/ntp.nix
+++ modules/features/ntp.nix
@@ -4,16 +4,16 @@
 }:
 {
   flake.nixosModules.ntp = { pkgs, ... }: {
-      services.chrony = {
-    enable = true;
-    servers = [
-      "0.fr.pool.ntp.org"
-      "1.fr.pool.ntp.org"
-      "2.fr.pool.ntp.org"
-      "3.fr.pool.ntp.org"
-    ];
-  };
-  time.timeZone = "Europe/Paris";
+    services.chrony = {
+      enable = true;
+      servers = [
+        "0.fr.pool.ntp.org"
+        "1.fr.pool.ntp.org"
+        "2.fr.pool.ntp.org"
+        "3.fr.pool.ntp.org"
+      ];
+    };
+    time.timeZone = "Europe/Paris";
 
   };
 }
