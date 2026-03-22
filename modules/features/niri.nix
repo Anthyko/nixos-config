3a9f30a2dbc37060d4f83ce2e168c4a8a7938a20
Anthyko <16465475+Anthyko@users.noreply.github.com>
2026-03-22 09:01:22 +0100

    migrated CommonDesktopModules

---
+++ modules/features/niri.nix
@@ -0,0 +1,20 @@
+{ inputs
+, lib
+, ...
+}:
+{
+  flake.nixosModules.niri = { pkgs, ... }: {
+  security.polkit.enable = true; # polkit
+  services.gnome.gnome-keyring.enable = true; # secret service
+  programs.niri.enable = true;
+  programs.niri.useNautilus = true;
+  programs.xwayland.enable = true;
+  stylix = {
+    enable = true;
+
+    # Palette Gruvbox Dark Medium
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
+ 
+  };
+  };
+}
