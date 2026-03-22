0a7c9a2e44d9c703f58a7980cba2fce593d8e6cb
Anthyko <16465475+Anthyko@users.noreply.github.com>
2026-03-22 08:49:54 +0100

    finished migrate commonBaseModules to base.nix

--- modules/profiles/base.nix
+++ modules/profiles/base.nix
@@ -1,13 +1,16 @@
 { inputs, ... }: {
 
   flake.nixosModules.base = {
-    imports = with inputs.self.nixosModules; [
+    imports = (with inputs.self.nixosModules; [
       dns-server
       ntp
       nix-config
       file-sync
       tailscale
       secrets
-    ];
+    ])++ [
+    inputs.home-manager.nixosModules.home-manager
+    inputs.sops-nix.nixosModules.sops
+      ];
   };
 }
