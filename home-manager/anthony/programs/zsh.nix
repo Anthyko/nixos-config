1a6704ebfa48fbff0aa0ac463d9b735e4da36c0d
Anthyko <16465475+Anthyko@users.noreply.github.com>
2026-04-28 18:45:44 +0200

    migrated to dependabot

--- home-manager/anthony/programs/zsh.nix
+++ home-manager/anthony/programs/zsh.nix
@@ -7,8 +7,6 @@
   imports = [ ../../common/programs/zsh-base.nix ];
   programs.zsh.shellAliases = {
     kde-fix-icons = "sed -i 's/file:\/\/\/nix\/store\/[^\/]*\/share\/applications\//applications:/gi' ~/.config/plasma-org.kde.plasma.desktop-appletsrc && systemctl restart --user plasma-plasmashell";
-    ghfb = "gh workflow run flake-bump";
-    ghfbl = "gh run list --workflow flake-bump.yml";
     ghb = "gh workflow run Build-configs";
     ghbl = "gh run list --workflow build.yml";
     nixos-repo = "xdg-open https://github.com/Anthyko/nixos-config";
