0010ce91f1c6502846f19c048fe2c46c7b442011
Anthyko <16465475+Anthyko@users.noreply.github.com>
2026-05-20 21:25:04 +0200

    migrated some config to the new hm module

--- home-manager/anthony/home.nix
+++ home-manager/anthony/home.nix
@@ -6,9 +6,6 @@ let
   common = import ../common/programs/common-pkgs.nix pkgs;
 in
 {
-  home.username = "anthony";
-  home.homeDirectory = "/home/anthony";
-  home.stateVersion = "24.05";
   home.packages =
     common
     ++ (with pkgs; [
@@ -65,13 +62,6 @@ in
 
     ]);
 
-  home.sessionVariables = {
-    LANG = "en_US.UTF-8";
-    LC_ALL = "en_US.UTF-8";
-  };
-
-  stylix.targets.kitty.enable = false;
-  stylix.targets.nixvim.enable = false;
   imports = [
     ./programs
     ../common/programs/git.nix
--- modules/users/anthony/default.nix
+++ modules/users/anthony/default.nix
@@ -7,5 +7,13 @@
       self.homeModules.base-desktop
       self.homeModules.multimedia-player
     ];
+    home.username = "anthony";
+    home.homeDirectory = "/home/anthony";
+    home.stateVersion = "24.05";
+    home.sessionVariables = {
+      LANG = "en_US.UTF-8";
+      LC_ALL = "en_US.UTF-8";
+    };
+    stylix.targets.nixvim.enable = false;
   };
 }
