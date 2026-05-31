38a8b83f1643ae0a74ce2451836ea0203d10122b
Anthyko <16465475+Anthyko@users.noreply.github.com>
2026-05-31 12:34:52 +0200

    fixed revan

--- modules/users/revan/default.nix
+++ modules/users/revan/default.nix
@@ -4,6 +4,7 @@
   flake = {
     homeConfigurations."revan" = inputs.home-manager.lib.homeManagerConfiguration {
       pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+      extraSpecialArgs = { inherit inputs; };
       modules = [
         self.homeModules.revan-module
       ];
