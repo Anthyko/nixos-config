5ae78a9e3b7a2ff0d6d565bffe80a8614808ba74
Anthyko <16465475+Anthyko@users.noreply.github.com>
2026-05-31 12:29:04 +0200

    docs

--- modules/mk.nix
+++ modules/mk.nix
@@ -1,5 +1,19 @@
 { inputs, self, ... }:
 {
+  # Builds a NixOS system configuration with Home Manager pre-configured.
+  # Abstracts away boilerplate (system, specialArgs, HM options) so each
+  # host only declares what makes it unique.
+  #
+  # Arguments:
+  #   modules - list of NixOS modules to include (profiles, host module, etc.)
+  #             Home Manager user configuration should be declared inside these modules.
+  #
+  # Example:
+  #   mkNixos [
+  #     self.nixosModules.base-desktop
+  #     self.nixosModules.zeno-module
+  #     { home-manager.users.anthony.imports = [ self.homeModules.anthony-module ]; }
+  #   ]
   _module.args.mkNixos =
     modules:
     inputs.nixpkgs.lib.nixosSystem {
