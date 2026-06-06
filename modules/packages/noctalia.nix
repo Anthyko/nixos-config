6b0831cc9f9d4455e3312071fd153f5a5f23f004
Anthony <16465475+Anthyko@users.noreply.github.com>
2026-06-06 14:37:35 +0200

    Update noctalia.nix

--- modules/packages/noctalia.nix
+++ modules/packages/noctalia.nix
@@ -4,7 +4,7 @@
     { pkgs, ... }:
     {
       packages.noctalia = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
-        inherit pkgs; # THIS PART IS VERY IMPORTAINT, I FORGOT IT IN THE VIDEO!!!
+        inherit pkgs;
         settings =
           (builtins.fromJSON
             # to save current noctalia config file run
