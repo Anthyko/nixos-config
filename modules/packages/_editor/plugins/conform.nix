293677308f1b6696372158530d793e44f4cd63fb
Anthony <16465475+Anthyko@users.noreply.github.com>
2026-07-03 17:03:54 +0200

    Update conform.nix

--- modules/packages/_editor/plugins/conform.nix
+++ modules/packages/_editor/plugins/conform.nix
@@ -15,18 +15,6 @@
     enable = true;
     settings = {
       notify_on_error = false;
-      format_on_save = ''
-        function(bufnr)
-          -- Disable "format_on_save lsp_fallback" for lanuages that don't
-          -- have a well standardized coding style. You can add additional
-          -- lanuages here or re-enable it for the disabled ones.
-          local disable_filetypes = { c = true, cpp = true, nix = true }
-          return {
-            timeout_ms = 2000,
-            lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype]
-          }
-        end
-      '';
       formatters_by_ft = {
         lua = [ "stylua" ];
         # Conform can also run multiple formatters sequentially
